51f10a2657f61c6cffcf2672b34cb3a908577338
liutao <cnliutao@gmail.com>
2012-08-28 07:54:36 +0800

    Scala 升级到 2.9.2, SBT 升级到 0.12.0

--- project/Build.scala
+++ project/Build.scala
@@ -6,13 +6,13 @@ object ApplicationBuild extends Build  {
 	private val buildSettings = Project.defaultSettings ++ Seq(
 		organization := "scala-demo",
 		version := "1.0-SNAPSHOT",
-		scalaVersion := "2.9.1",
+		scalaVersion := "2.9.2",
 		shellPrompt := {
 			(state: State) => "%s> ".format(Project.extract(state).currentProject.id)
 		},
 		scalacOptions ++= Seq("-encoding", "UTF-8", "-Xlint","-deprecation", "-unchecked"),
         javacOptions  ++= Seq("-source","1.6","-target","1.6", "-encoding", "UTF-8","-Xlint:unchecked", "-Xlint:deprecation"),
-        libraryDependencies := Seq(
+        libraryDependencies ++= Seq(
         		"org.specs2" %% "specs2" % "1.11" % "test"
         	)
 	)
